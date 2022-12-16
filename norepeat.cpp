61055e0baec4603059f11c1bd6308c2fe4e29c4e
Sergey Shchegolikhin <sshchegolikhin@gmail.com>
2022-12-16 12:33:59 +0300

    Create norepeat.cpp

---
+++ norepeat.cpp
@@ -0,0 +1,59 @@
+int main()
+{
+	string s = "abbbaa"; // -> ababab
+
+	map<char, int> m;
+	for (auto c : s)
+	{
+		if (!m.contains(c))
+		{
+			m.insert({ c, 0 });
+		}
+		m[c]++;
+	}
+
+	list<char> res;
+	while (!m.empty())
+	{
+		for (auto i = m.begin(); i != m.end() && !m.empty();)
+		{
+			if ((i)->second != 0)
+			{
+				res.push_back(i->first);
+				--(i->second);
+			}
+			if (m.size() == 1)
+			{
+				for (auto j = ++(res.begin()), t = res.begin();
+					j != res.end() && i->second != 0; ++t)
+				{
+					if (*j != i->first && *t != i->first)
+					{
+						res.insert(j++, i->first);
+						--(i->second);
+						++t;
+					}
+					else ++j;
+				}
+				for (int j = 0; j < i->second; j++)
+				{
+					res.push_back(i->first);
+				}
+				i->second = 0;
+			}
+
+			if (i->second == 0) m.erase(i++);
+			else { i++; }
+		}
+	}
+	
+	char t = *(++res.begin());
+	bool ok = 1;
+	for (auto x : res)
+	{
+		if (t == x) ok = 0;
+		t = x;
+	}
+	if (ok) for (auto x : res) cout << x;
+	else cout << "Not ok!";
+}
